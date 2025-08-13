61915eeea1914455c6eb22380d4710671396d068
earwin <earwin95@gmail.com>
2025-08-13 20:25:34 +0200

    Ajout des formulaires de modification de profil avec gestion des champs nom d'utilisateur et bio.

---
+++ src/Form/ProfileEditType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfileEditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'label' => 'Nom d\'utilisateur',
+            ])
+            ->add('bio', TextareaType::class, [
+                'label' => 'Bio',
+                'required' => false,
+            ]);
+        // Tu peux ajouter l'email si tu veux le rendre modifiable :
+        // ->add('email')
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
