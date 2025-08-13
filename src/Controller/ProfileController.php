e07da61873c88c38259c62eb76016ebe01b51e7b
earwin <earwin95@gmail.com>
2025-08-13 20:26:05 +0200

    Amélioration de la page de profil : réorganisation du code HTML, ajout de styles CSS et mise à jour des liens pour une meilleure expérience utilisateur.

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,20 +2,46 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\ProfileEditType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/profile')]
 class ProfileController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profile')]
+    #[Route('', name: 'app_profile')]
     public function index(): Response
     {
-        // Bloque l'accès si l'utilisateur n'est pas connecté ou n'a pas le bon rôle
         $this->denyAccessUnlessGranted('ROLE_USER');
 
         return $this->render('profile/index.html.twig', [
             'user' => $this->getUser(),
+        ]);
+    }
+
+    #[Route('/edit', name: 'app_profile_edit')]
+    public function edit(Request $request, EntityManagerInterface $em): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $user = $this->getUser();
+
+        $form = $this->createForm(ProfileEditType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            $this->addFlash('success', 'Profil mis à jour avec succès.');
+            return $this->redirectToRoute('app_profile');
+        }
+
+        return $this->render('profile/edit.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
