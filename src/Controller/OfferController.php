fda352bb63ab5376c18a14e70180f3bdae1b1c26
earwin <earwin95@gmail.com>
2025-08-19 21:15:12 +0200

    Ajout des fonctionnalités de gestion des offres : création, modification et suppression d'offres, avec des contrôles d'accès et des formulaires associés.

--- src/Controller/OfferController.php
+++ src/Controller/OfferController.php
@@ -2,17 +2,91 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Offer;
+use App\Form\OfferType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-final class OfferController extends AbstractController
+class OfferController extends AbstractController
 {
-    #[Route('/offer', name: 'app_offer')]
-    public function index(): Response
+    #[Route('/offres/nouvelle', name: 'offer_new')]
+    public function new(Request $request, EntityManagerInterface $em): Response
     {
-        return $this->render('offer/index.html.twig', [
-            'controller_name' => 'OfferController',
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $offer = new Offer();
+        $offer->setOwner($this->getUser());
+        $offer->setStatus('active');
+
+        $form = $this->createForm(OfferType::class, $offer);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($offer);
+            $em->flush();
+
+            $this->addFlash('success', 'Offre créée avec succès !');
+            return $this->redirectToRoute('offer_index');
+        }
+
+        return $this->render('offer/new.html.twig', [
+            'form' => $form->createView(),
         ]);
+    }
+
+    #[Route('/offres', name: 'offer_index')]
+    public function index(EntityManagerInterface $em): Response
+    {
+        $offers = $em->getRepository(Offer::class)->findAll();
+
+        return $this->render('offer/index.html.twig', [
+            'offers' => $offers,
+        ]);
+    }
+
+    #[Route('/offres/{id}/modifier', name: 'offer_edit')]
+    public function edit(Offer $offer, Request $request, EntityManagerInterface $em): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        if ($offer->getOwner() !== $this->getUser()) {
+            throw $this->createAccessDeniedException('Vous ne pouvez modifier que vos propres offres.');
+        }
+
+        $form = $this->createForm(OfferType::class, $offer);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'Offre modifiée avec succès !');
+
+            return $this->redirectToRoute('offer_index');
+        }
+
+        return $this->render('offer/edit.html.twig', [
+            'form' => $form->createView(),
+            'offer' => $offer,
+        ]);
+    }
+
+    #[Route('/offres/{id}/supprimer', name: 'offer_delete', methods: ['POST'])]
+    public function delete(Offer $offer, Request $request, EntityManagerInterface $em): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        if ($offer->getOwner() !== $this->getUser()) {
+            throw $this->createAccessDeniedException('Vous ne pouvez supprimer que vos propres offres.');
+        }
+
+        if ($this->isCsrfTokenValid('delete_offer_' . $offer->getId(), $request->request->get('_token'))) {
+            $em->remove($offer);
+            $em->flush();
+            $this->addFlash('success', 'Offre supprimée avec succès.');
+        }
+
+        return $this->redirectToRoute('offer_index');
     }
 }
